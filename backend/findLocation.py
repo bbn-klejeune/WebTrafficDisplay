4c0e3361a1a4631dc144893380eaae1c50927a42
Kyle Lejeune <klejeune@bbnmedia.org>
2023-03-15 16:29:06 -0400

    Added db functionality

--- backend/findLocation.py
+++ backend/findLocation.py
@@ -1,9 +1,25 @@
 import http.client
 import time
 import json
+import pymongo
+
+def formatDBEntry(entry):
+
+    ipDict = {"ip_address": entry["ip"],
+              "latitude": entry["location"]["latitude"],
+              "longitude": entry["location"]["longitude"],
+              "flag": entry["country"]["flag"]["file"],
+              "country": entry["country"]["code"],
+              "state": entry["area"]["name"],
+              "city": entry["city"]["name"],
+              "timezone": entry["time"]["timezone"]}
+
+    return ipDict
 
 #IP Geo Location API
-conn = http.client.HTTPSConnection("ip-geo-location.p.rapidapi.com")
+conn     = http.client.HTTPSConnection("ip-geo-location.p.rapidapi.com")
+bbnAppDB = pymongo.MongoClient("mongodb://localhost:27017/")["bbnApp"]
+locations = bbnAppDB["locations"]
 
 # API Request Header
 headers = {
@@ -14,16 +30,18 @@ headers = {
 ipList = open('.\\log\\cleanIP.txt')
 
 for ip in ipList:
-    ip = ip.strip()
-    print(ip)
-    requestString = f'/ip/{ip}?format=json'
+    requestString = f'/ip/{ip.strip()}?format=json'
 
+    # print(ip, end="\n\n") #Debugging 
     conn.request("GET", requestString, headers=headers)
     res = conn.getresponse()
     data = json.loads(res.read())
-    latlng = data["location"]["latitude"] + data["location"]["longitude"]
-    currentIp = data["ip"]
-    print(currentIp, ':', latlng)
-    time.sleep(1)
+    # print(data, end="\n\n") #Debugging
+    record = formatDBEntry(data)
+    # print(record, end="\n\n\n\n\n\n") #Debugging
+    locations.insert_one(record)
+    print("Loading. . .")
+    print(record, end="\n\n\n\n\n\n\n\n")
+    time.sleep(3)
 
 conn.close()
